afe566c40829b7373336b7b80567b1fb91e7a4a5
Steven yang <steven_yang9190@hotmail.com>
2016-04-12 19:00:28 -0700

    first commit

--- TimesTable/ViewController.swift
+++ TimesTable/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sliderOutlet: UISlider!
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        numberLabel.text = "Multiply by \(String(Int(sliderOutlet.value)))"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "\(indexPath.row + 1): \(indexPath.row + 1) * \(Int(sliderOutlet.value)) = \(String((indexPath.row + 1) * Int(sliderOutlet.value)))"
+        return cell
+    }
+
+    @IBAction func numberSlider(sender: AnyObject) {
+        numberLabel.text = String(Int(sliderOutlet.value))
+        tableView.reloadData()
+    }
 
 }
 
